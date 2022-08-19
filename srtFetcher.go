f49a54db310530ad2c124bc665706a4597a99034
F Santos <filipesantos@tutanota.com>
2022-08-19 18:52:15 +0100

    added: subtitles struct, strFetcher function and renamed getSubtitles function to getSubFiles

--- srtFetcher.go
+++ srtFetcher.go
@@ -6,7 +6,52 @@ import (
 	"strings"
 )
 
-func getSubtitles(dir string) []string {
+type subtitles struct {
+	folderName string
+	subFiles   []string
+}
+
+// srtFetcher fetches the subtitles from a subs directory,
+// there are two cases:
+// 1) if there is a single video file, all subtitles should be in a directory called subs
+// 2) if there are multiple video files, there will be a folder of subtitles for each video
+// inside the subs folder
+func srtFetcher(dir string) []subtitles {
+	// first case
+	dirSubs := getSubFiles(dir)
+	if len(dirSubs) > 0 {
+		dirSubtitles := make([]subtitles, 1)
+		dirSubtitles[0] = subtitles{
+			folderName: "Subs",
+			subFiles:   dirSubs,
+		}
+		return dirSubtitles
+	}
+
+	// second case
+	dirContent, err := os.ReadDir(dir)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var folderSubtitles []subtitles
+	for _, file := range dirContent {
+		if !file.IsDir() {
+			continue
+		}
+		fileSubs := getSubFiles(dir + string(os.PathSeparator) + file.Name())
+		if len(fileSubs) > 0 {
+			fileSubtitles := subtitles{
+				folderName: file.Name(),
+				subFiles:   fileSubs,
+			}
+			folderSubtitles = append(folderSubtitles, fileSubtitles)
+		}
+	}
+	return folderSubtitles
+}
+
+func getSubFiles(dir string) []string {
 	var result []string
 
 	dirContent, err := os.ReadDir(dir)
@@ -24,7 +69,18 @@ func getSubtitles(dir string) []string {
 }
 
 func srtFetcherTest() {
+	// flagsOrDir := os.Args[1:]
+	// mySubs := getSubs(flagsOrDir[0])
+	// fmt.Println(mySubs)
+
 	flagsOrDir := os.Args[1:]
-	mySubs := getSubtitles(flagsOrDir[0])
-	fmt.Println(mySubs)
+	mySubs := srtFetcher(flagsOrDir[0])
+	for _, sub := range mySubs {
+		fmt.Println("---------------------------")
+		fmt.Println("sub folder:", sub.folderName)
+		fmt.Println("Subtitles:")
+		for i, subFile := range sub.subFiles {
+			fmt.Println(i, "=", subFile)
+		}
+	}
 }
