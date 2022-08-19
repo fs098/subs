791ec300055c418fa1f6c8bf3dc0098339e04fb2
F Santos <filipesantos@tutanota.com>
2022-08-19 22:59:45 +0100

    added: srtCopy and copy functions, removed: unused field from videoInfo struct (path)

--- srtCopy.go
+++ srtCopy.go
@@ -3,13 +3,42 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 )
 
 type videoInfo struct {
 	name         string // without termination
-	path         string
 	subtitleName string
+	// path      string
+
+}
+
+func srtCopy(dir string) {
+	var videoInfos []videoInfo
+	getVideoInfo(dir, &videoInfos)
+	subs := findSubsFileAndGetSubtitles(dir)
+
+	var counter int
+	if len(subs) == 1 {
+		copy(videoInfos[0].subtitleName, subs[0].subFiles[0])
+		counter++
+		fmt.Println("Copied", counter, "files!")
+		return
+	}
+
+	for i, info := range videoInfos {
+		if info.name == subs[i].folderName {
+			copy(info.subtitleName, subs[i].subFiles[0])
+			counter++
+		}
+	}
+	fmt.Println("Copied", counter, "files!")
+}
+
+func copy(destFile string, srcFile string) {
+	cmd := exec.Command("cp", srcFile, destFile)
+	cmd.Run()
 }
 
 func getVideoInfo(dir string, infos *[]videoInfo) {
@@ -25,8 +54,9 @@ func getVideoInfo(dir string, infos *[]videoInfo) {
 		if isVideo(file.Name()) {
 			newInfo := videoInfo{
 				name:         getVideoName(file.Name()),
-				path:         dir + string(os.PathSeparator) + file.Name(),
 				subtitleName: dir + string(os.PathSeparator) + getVideoName(file.Name()) + ".srt",
+				// path:      dir + string(os.PathSeparator) + file.Name(),
+
 			}
 			*infos = append(*infos, newInfo)
 		}
@@ -52,20 +82,23 @@ func getVideoName(name string) string {
 }
 
 func strCopyTest() {
-	// cmd := exec.Command("cp", "oldDir", "newDir")
-	// cmd.Run()
-
 	// test1 := getVideoName("test1.mkv")
 	// test2 := getVideoName("test2.mp4")
 	// fmt.Println(test1, test2)
 
+	// flagsOrDir := os.Args[1:]
+	// myVids := []videoInfo{}
+	// getVideoInfo(flagsOrDir[0], &myVids)
+	// for _, vid := range myVids {
+	// 	fmt.Println("---------------------------")
+	// 	fmt.Println("name:", vid.name)
+	// 	fmt.Println("path:", vid.path)
+	// 	fmt.Println("sub name:", vid.subtitleName)
+	// }
+
 	flagsOrDir := os.Args[1:]
-	myVids := []videoInfo{}
-	getVideoInfo(flagsOrDir[0], &myVids)
-	for _, vid := range myVids {
-		fmt.Println("---------------------------")
-		fmt.Println("name:", vid.name)
-		fmt.Println("path:", vid.path)
-		fmt.Println("sub name:", vid.subtitleName)
-	}
+	srtCopy(flagsOrDir[0])
+
+	// filesDeleted := srtDeleter(flagsOrDir[0])
+	// fmt.Println(filesDeleted, "files deleted!")
 }
